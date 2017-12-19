4a3ba6be3e107c71f9809df81b462956b4d27da2
kitiwat chanluthin <aurelia@kitiwats-MacBook-Pro.local>
2017-12-19 14:24:59 +0700

    worked with stackview

---
+++ Adaptive Interface1/Controller/HiddenLabelVC.swift
@@ -0,0 +1,42 @@
+//
+//  HiddenLabelVC.swift
+//  Adaptive Interface1
+//
+//  Created by kitiwat chanluthin on 19/12/17.
+//  Copyright © 2017 kitiwat chanluthin. All rights reserved.
+//
+
+import UIKit
+
+class HiddenLabelVC: UIViewController {
+
+    @IBOutlet weak var withDealLbl: UILabel!
+    @IBOutlet weak var withDealNumLbl: UILabel!
+    @IBOutlet weak var youSaveLbl: UILabel!
+    @IBOutlet weak var youSaveNumLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+    @IBAction func runitTapped(_ sender: Any) {
+        self.withDealLbl.isHidden = !self.withDealLbl.isHidden
+        self.withDealNumLbl.isHidden = !self.withDealNumLbl.isHidden
+        
+        self.youSaveLbl.isHidden = !self.youSaveLbl.isHidden
+        
+        self.youSaveNumLbl.isHidden = !self.youSaveNumLbl.isHidden
+       // print("tap")
+    }
+    
+}
